6851ede481bb013c2074101f3ddaa249b9841333
wongpp <934685522@qq.com>
2019-03-05 13:33:47 +0800

    c

--- deepsleep/algorithms/algorithms.py
+++ deepsleep/algorithms/algorithms.py
@@ -1,28 +1,46 @@
 import numpy as np
-
+from sklearn.preprocessing import Binarizer
 class FeatureAlgorithms:
-
     def simpleLZ(self, X):
+        Xs = X.copy()
+        # if LZv <= 1:
+        #     raise ValueError('invalid argument')
+        # 二值化
+        Xmean = np.mean(Xs)
+        Xbin = Xs
+        for j, Xj in enumerate(Xs) :
+            if Xj >= Xmean:
+                Xbin[j] = 1
+            else:
+                Xbin[j] = 0
+        # 转换成int型
+        Xbin = np.asanyarray(Xbin,dtype=int)
+        # 初始化子串
         c = 1
-        S = X[0]
+        S = str(Xbin[0])
         Q = ''
         SQ = ''
-        for i, x in enumerate(X):
+        # 找到新子串的数目
+        for i, x in enumerate(Xbin):
             if i >= 1:
-                Q = Q + x
+                Q = Q + str(x)
                 SQ = S + Q
                 SQv = SQ[0: -1]
                 if Q not in SQv:
                     S = SQ
                     Q = ''
                     c = c + 1
-        b = len(X) / np.log2(len(X))
+        #
+        b = len(Xs) / np.log2(len(Xs))
         lzc = c / b
         return lzc
 
+    def multiscaleLZ(self, X, coarseLevel):  # 粗粒化水平
+        if coarseLevel >= 2:
+            raise ValueError('invalid argument')
 
-# X = '10101010'
-# sLz = FeatureAlgorithm()
-# sLz.simpleLZ(X)
+        return
+
+
 
 
--- deepsleep/testAlgorithms.py
+++ deepsleep/testAlgorithms.py
@@ -1,5 +1,21 @@
 from deepsleep.algorithms import algorithms
-
-X = '10101010'
+import numpy as np
+# X = np.array([1,0,1,0,1,-6,1,0,0.6])
+# X = np.array([0, 0, 1, 1, 0, 1, 1, 1, 0])
+X = np.arange(1, 10)
 sLz = algorithms.FeatureAlgorithms()
-sLz.simpleLZ (X)
+LZv = sLz.simpleLZ(X)
+
+# if LZv <= 1:
+#     raise ValueError('invalid argument')
+
+# for j in X:
+#     print(j[0])
+# from sklearn.preprocessing import Binarizer
+#
+# biner = Binarizer(threshold = np.mean(X) )
+# X_bin = biner.transform(X)
+
+# X.reshape(-1, 1)
+# biner = Binarizer()
+# X = biner.transform(X)
