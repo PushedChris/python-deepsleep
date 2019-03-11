3f8c200f52dbb1ee369ba0d6c0faf2e1299306d6
wongpp <934685522@qq.com>
2019-03-11 15:40:02 +0800

    multiscaleLZ

--- deepsleep/algorithms/algorithms.py
+++ deepsleep/algorithms/algorithms.py
@@ -1,9 +1,8 @@
 import numpy as np
-from sklearn.preprocessing import Binarizer
+# from sklearn.preprocessing import Binarizer
 
 
 class FeatureAlgorithms:
-
 
     def findBin(self, X, si):
         # 二值化
@@ -13,11 +12,12 @@ class FeatureAlgorithms:
         Xsmall = []
         for j, Xj in enumerate(Xs):
             if Xj >= Xmean:
-                Xbig = Xbig+[j] # 保存下标
+                Xbig = Xbig + [j]  # 保存下标
             else:
-                Xsmall = Xsmall+[j]
-        si = si+1
-        return  {'x0':Xsmall, 'x1':Xbig, 'slice_i':si}
+                Xsmall = Xsmall + [j]
+        si = si + 1
+
+        return {'x0': Xsmall, 'x1': Xbig, 'slice_i': si}
 
     def simpleLZ(self, X):
         Xs = X.copy()
@@ -54,21 +54,75 @@ class FeatureAlgorithms:
         return lzc
 
     def multiscaleLZ(self, X, coarseLevel):  # coarseLevel：粗粒化水平
+        global xflagold, xflag, xold
         if coarseLevel <= 2 or coarseLevel % 2 != 0:  # 不是偶数
             raise ValueError('invalid argument: coarseLevel')
 
         Xs = np.reshape(X.copy(), (1, np.size(X)))
-        Xmean = np.mean(Xs)
-        Xbin = np.zeros(np.shape(Xs))
-        slice = np.asanyarray(coarseLevel / 2, dtype=int)
-        Xbig = np.zeros((slice, np.size(Xs)))
-        Xsmall = np.zeros((slice, np.size(Xs)))
-        Xs = np.r_[Xs, Xbig]
-        slice_i=0
+
+        Xbin = np.zeros(np.shape(Xs),dtype=int)
+        slice = int(coarseLevel / 2)
+        # Xbig = np.zeros((slice, np.size(Xs)))
+        # Xsmall = np.zeros((slice, np.size(Xs)))
+        # Xs = np.r_[Xs, Xbig]
+        slice_i = 0
         # for slice_i in range(0, slice):  # 0 1
         # x0, x1 = self.findBin(Xs[0])
+        # si = 0
+        # binSeries = {'x0': Xsmall, 'x1': Xbig, 'slice_i': slice_i}
+        binSeries = self.findBin(Xs[0], slice_i)
+        binSeries_temp0 = self.findBin(Xs[0][binSeries['x0']], binSeries['slice_i'])
+        binSeries_temp1 = self.findBin(Xs[0][binSeries['x1']], binSeries['slice_i'])
 
-        x0, x1, slice_i  = self.findBin(Xs[0][x0], slice_i)
-        x0, x1 = self.findBin(Xs[0][x1])
+        s00 = np.array(binSeries_temp0['x0'])
+        s01 = np.array(binSeries_temp0['x1'])
+        s10 = np.array(binSeries_temp1['x0'])
+        s11 = np.array(binSeries_temp1['x1'])
+
+        s0 = np.array(binSeries['x0'])
+        s1 = np.array(binSeries['x1'])
+
+        x0 = s0[s00]
+        x1 = s0[s01]
+        x2 = s1[s10]
+        x3 = s1[s11]
+
+        X0 = Xs[0][x0]
+        X1 = Xs[0][x1]
+        X2 = Xs[0][x2]
+        X3 = Xs[0][x3]
+        X0mean = np.mean(X0)
+        X1mean = np.mean(X1)
+        X2mean = np.mean(X2)
+        X3mean = np.mean(X3)
+        Xmean = np.mean(Xs)
+
+        # step2 : 将序列的第一个值与平均值比较，第二个值开始二值化结果取决于与前一个点比较
+        for i, x in enumerate(Xs[0]):
+            if x in X0:
+                xflag = 0
+            elif x in X1:
+                xflag = 1
+            elif x in X2:
+                xflag = 2
+            elif x in X3:
+                xflag = 3
+
+            if i >= 1:
+                if x < xold and (xflag != xflagold):
+                    Xbin[0][i] = 0
+                elif x >= xold and (xflag != xflagold ):
+                    Xbin[0][i] = 1
+                else:
+                    Xbin[0][i] = Xbin[0][i-1]
+                xold = x
+                xflagold = xflag
+            elif i == 0 :
+                if x < Xmean:
+                    Xbin[0][i] = 0
+                elif x >= Xmean:
+                    Xbin[0][i] = 1
+                xold = x
+                xflagold = xflag
 
         return Xbin
--- deepsleep/testAlgorithms.py
+++ deepsleep/testAlgorithms.py
@@ -1,8 +1,10 @@
 from deepsleep.algorithms import algorithms
 import numpy as np
+import random
 # X = np.array([1,0,1,0,1,-6,1,0,0.6])
 # X = np.array([0, 0, 1, 1, 0, 1, 1, 1, 0])
-X = np.arange(3, 16,0.2)
+X = np.random.random(100)
+
 sLz = algorithms.FeatureAlgorithms()
 LZv = sLz.multiscaleLZ(X,4)
 
