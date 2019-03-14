c446c47e68b03045d11d90a7abaf813cfe090233
wongpp <934685522@qq.com>
2019-03-14 10:02:05 +0800

    a

--- deepsleep/algorithms/algorithms.py
+++ deepsleep/algorithms/algorithms.py
@@ -1,12 +1,19 @@
 import numpy as np
 # from sklearn.preprocessing import Binarizer
-from scipy.special import perm
-from itertools import combinations, permutations
+# from scipy.special import perm
+from itertools import permutations
+
 
 class FeatureAlgorithms:
 
     @staticmethod
     def _meanslice(X, si):
+        """
+        功能: 利用平均值进行划分
+        参数：   X：带划分的数据序列
+                si：粗粒化水平
+        输出： 划分出的两个区域的下标，序列被划分的次数
+        """
         # 二值化
         Xs = X.copy()  # 没有二维化
         Xmean = np.mean(Xs)
@@ -20,7 +27,14 @@ class FeatureAlgorithms:
         si = si + 1
         return {'x0': Xsmall, 'x1': Xbig, 'slice_i': si}
 
-    def simpleLZ(self, X):
+    @staticmethod
+    def simpleLZ(X):
+        """
+        功能: 最简单Lemple-Ziv复杂度计算
+        参数：   X：原始数据序列
+
+        输出： 复杂度的值
+        """
         Xs = np.reshape(X.copy(), (1, np.size(X)))  # 二维化
         Xmean = np.mean(Xs)
         Xbin = np.zeros(np.shape(Xs), dtype=int)  # 二值化序列初始化
@@ -34,7 +48,6 @@ class FeatureAlgorithms:
         c = 1
         S = str(Xbin[0][0])
         Q = ''
-        SQ = ''
         # 找到新子串的数目
         for i, x in enumerate(Xbin[0]):
             if i >= 1:
@@ -50,13 +63,19 @@ class FeatureAlgorithms:
         lzc = c / b
         return lzc
 
-    def coarseLZ(self, X, coarseLevel):  # coarseLevel：粗粒化水平
+    def coarseLZ(self, X, coarseLevel):
+        """
+        功能: 粗粒化Lemple-Ziv复杂度计算
+        参数：   X：原始数据序列
+                coarseLevel：粗粒化水平
+        输出： 复杂度的值
+        """
         global xflagold, xflag, xold
         if coarseLevel <= 2 or coarseLevel % 2 != 0:  # 不是偶数
             raise ValueError('invalid argument: coarseLevel')
         Xs = np.reshape(X.copy(), (1, np.size(X)))
-        Xbin = np.zeros(np.shape(Xs),dtype=int)
-        slice = int(coarseLevel / 2)
+        Xbin = np.zeros(np.shape(Xs), dtype=int)
+        # slice = int(coarseLevel / 2)
         # Xbig = np.zeros((slice, np.size(Xs)))
         # Xsmall = np.zeros((slice, np.size(Xs)))
         # Xs = np.r_[Xs, Xbig]
@@ -104,10 +123,10 @@ class FeatureAlgorithms:
             if i >= 1:
                 if x < xold and (xflag != xflagold):
                     Xbin[0][i] = 0
-                elif x >= xold and (xflag != xflagold ):
+                elif x >= xold and (xflag != xflagold):
                     Xbin[0][i] = 1
                 else:
-                    Xbin[0][i] = Xbin[0][i-1]
+                    Xbin[0][i] = Xbin[0][i - 1]
 
             elif i == 0:
                 if x < Xmean:
@@ -122,7 +141,6 @@ class FeatureAlgorithms:
         c = 1
         S = str(Xbin[0][0])
         Q = ''
-        SQ = ''
         # 找到新子串的数目
         for i, x in enumerate(Xbin[0]):
             if i >= 1:
@@ -139,10 +157,17 @@ class FeatureAlgorithms:
 
         return lzc
 
-    def DiffLZ(self, X):
+    @staticmethod
+    def diffLZ(X):
+        """
+        功能: 差分Lemple-Ziv复杂度计算
+        参数：   X：原始数据序列
+
+        输出： 复杂度的值
+        """
         global xold
         Xs = np.reshape(X.copy(), (1, np.size(X)))  # 二维化
-        Xbin = np.zeros(np.shape(Xs),dtype=int)  # 二值化序列初始化
+        Xbin = np.zeros(np.shape(Xs), dtype=int)  # 二值化序列初始化
         # step1： 全序列差分绝对值，再平均，
         D = abs(Xs[0][:-1] - Xs[0][1:])
         T = np.sum(D) / len(D)
@@ -151,11 +176,11 @@ class FeatureAlgorithms:
         Xmean = np.mean(Xs)
         for i, x in enumerate(Xs[0]):
             if i >= 1:
-                if D[i-1] < T:
-                    Xbin[0][i] = Xbin[0][i-1]
-                elif D[i-1] >= T:
+                if D[i - 1] < T:
+                    Xbin[0][i] = Xbin[0][i - 1]
+                elif D[i - 1] >= T:
                     if x < xold:
-                       Xbin[0][i] = 0
+                        Xbin[0][i] = 0
                     elif x >= xold:
                         Xbin[0][i] = 1
             else:
@@ -170,7 +195,6 @@ class FeatureAlgorithms:
         c = 1
         S = str(Xbin[0][0])
         Q = ''
-        SQ = ''
         # 找到新子串的数目
         for i, x in enumerate(Xbin[0]):
             if i >= 1:
@@ -186,17 +210,24 @@ class FeatureAlgorithms:
         lzc = c / b
         return lzc
 
-    def multiLZ(self, X, window):
+    @staticmethod
+    def multiLZ(X, window):
+        """
+        功能: 多尺度Lemple-Ziv复杂度计算
+        参数：   X：原始数据序列
+                window：滑窗长度
+        输出： 复杂度的值
+        """
         global xold
         if window <= 4 or window % 2 == 0:  # 是偶数
             raise ValueError('invalid argument: window')
         Xs = np.reshape(X.copy(), (1, np.size(X)))  # 二维化
-        Xbin = np.zeros(np.shape(Xs),dtype=int)  # 二值化序列初始化
-        Tdw = np.zeros(np.shape(Xs),dtype=float)
+        Xbin = np.zeros(np.shape(Xs), dtype=int)  # 二值化序列初始化
+        Tdw = np.zeros(np.shape(Xs), dtype=float)
         # step1： 滑窗中点
         for i, x in enumerate(Xs[0]):
-            if i >= (window-1)/2 and i <=  + len(Xs[0])-1-((window-1)/2):
-                Tdw[0][i] = np.median(Xs[0][i-2:i+3])
+            if (window - 1) / 2 <= i <= len(Xs[0]) - 1 - ((window - 1) / 2):
+                Tdw[0][i] = np.median(Xs[0][i - 2: i + 3])
             else:
                 Tdw[0][i] = Xs[0][i]
 
@@ -212,7 +243,6 @@ class FeatureAlgorithms:
         c = 1
         S = str(Xbin[0][0])
         Q = ''
-        SQ = ''
         # 找到新子串的数目
         for i, x in enumerate(Xbin[0]):
             if i >= 1:
@@ -227,29 +257,31 @@ class FeatureAlgorithms:
         b = len(Xs[0]) / np.log2(len(Xs[0]))
         lzc = c / b
         return lzc
-    def permuteLZ(self, X, t, m):
+
+    @staticmethod
+    def permuteLZ(X, t, m):
         """
         功能: 排列Lemple-Ziv复杂度计算
         参数：   X：原始数据序列
                 t： 样本点的迟滞因子
-                m： 排列模式中包含的样本数目
+                m： 排列模式中包含的样本数目,又叫“嵌入维数”
         输出： 复杂度的值
         """
-        if t < 1 and m < 3:  #参数异常
+        if t < 1 and m < 3:  # 参数异常
             raise ValueError('invalid argument: t  and  m')
         Xs = np.reshape(X.copy(), (1, np.size(X)))  # 二维化
-        Xsymbol = np.zeros(np.shape(Xs),dtype=int)  # 符号化序列初始化 （符号：1 2 3 4 5 6）
+        Xsymbol = np.zeros(np.shape(Xs), dtype=int)  # 符号化序列初始化 （符号：1 2 3 4 5 6）
 
-        #step1: 将信号转换成一个排列序列,  （m!）种排列模式，
-        lenXs = len(Xs[0])  #原始数据序列的长度
-        permlist = list(permutations(range(0, m), m)) # 生成排列模式
+        # step1: 将信号转换成一个排列序列,  （m!）种排列模式，
+        lenXs = len(Xs[0])  # 原始数据序列的长度
+        permlist = list(permutations(range(0, m), m))  # 生成排列模式
         # motif = np.zeros((len(permlist),), dtype=int)
 
         Xsend = range(lenXs)
         for j in Xsend:
-            if j >= (t * m):
+            if j >= (t * m): # 前面几个值不考虑
                 # 重构序列是细粒度平滑移动（重叠）
-                Xsvec = Xs[0][j-t*m: j: t]
+                Xsvec = Xs[0][j - t * m: j: t]
                 # Xmotif = np.sort(Xsvec)
                 Xmotif = np.argsort(Xsvec)
                 for jj in range(len(permlist)):
@@ -263,7 +295,6 @@ class FeatureAlgorithms:
         c = 1
         S = str(Xsymbol[0][0])
         Q = ''
-        SQ = ''
         # 找到新子串的数目
         for i, x in enumerate(Xsymbol[0]):
             if i >= 1:
@@ -277,35 +308,34 @@ class FeatureAlgorithms:
 
         b = len(Xs[0]) / np.log2(len(Xs[0]))
         lzc = c / b
-        return
+        return lzc
 
-
-    def permuteEntropy(self, X, t, m):
+    @staticmethod
+    def permuteEntropy(X, t, m):
         """
         功能: 排列熵计算
         参数：   X：原始数据序列
                 t： 样本点的迟滞因子
                 m： 排列模式中包含的样本数目
-
         """
         if t < 1 and m < 3:  # 参数异常
             raise ValueError('invalid argument: t  and  m')
         Xs = np.reshape(X.copy(), (1, np.size(X)))  # 二维化
 
-        #step1: 将信号转换成一个排列序列,  （m!）种排列模式，
+        # step1: 将信号转换成一个排列序列,  （m!）种排列模式，
         lenXs = len(Xs[0])
         permlist = list(permutations(range(0, m), m))
         motif = np.zeros((len(permlist),), dtype=int)
 
         Xend = range(lenXs)
         for j in Xend:
-            if j >= (t * (m)):
-                Xsvec = Xs[0][j-t*(m): j:t]
+            if j >= (t * m):
+                Xsvec = Xs[0][j - t * m: j:t]
                 # Xmotif = np.sort(Xsvec)
                 Xmotif = np.argsort(Xsvec)
                 for jj in range(len(permlist)):
                     model = np.array(permlist[jj])
-                    if np.all(np.equal(model, Xmotif)) :
+                    if np.all(np.equal(model, Xmotif)):
                         motif[jj] = motif[jj] + 1
                         break
         # hist = motif.copy()
--- deepsleep/testAlgorithms.py
+++ deepsleep/testAlgorithms.py
@@ -2,22 +2,13 @@ from deepsleep.algorithms import algorithms
 import numpy as np
 import random
 # X = np.array([1,0,1,0,1,-6,1,0,0.6])
-# X = np.array([0, 0, 1, 1, 0, 1, 1, 1, 0])
-X = np.random.random(1200)*100
+# X = np.array([1, 0,0,1,1,1,1,0,1,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0])
+X = np.random.random(3200)*100
 
 sLz = algorithms.FeatureAlgorithms()
-LZv = sLz.permuteLZ(X, 2, 3)
+LZv = sLz.permuteLZ(X)
 
-# if LZv <= 1:
-#     raise ValueError('invalid argument')
-
-# for j in X:
-#     print(j[0])
-# from sklearn.preprocessing import Binarizer
-#
-# biner = Binarizer(threshold = np.mean(X) )
-# X_bin = biner.transform(X)
-
-# X.reshape(-1, 1)
-# biner = Binarizer()
-# X = biner.transform(X)
+# import nolds
+# randata= np.random.random(10000) # 1000个随机点
+# rwalk = np.cumsum(randata)  #累积和
+# h = nolds.corr_dim(rwalk,5)
